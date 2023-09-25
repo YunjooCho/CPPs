81c45ea0e39dd5227d78335734b9ae420a4f8892
YUNJOO CHO <yunjoocho@YUNJOOui-MacBookAir.local>
2023-09-25 14:36:17 +0900

    CPP06 - Update ex02

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 22:29:37 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/09/24 22:57:05 by yunjcho          ###   ########seoul.kr  */
+/*   Updated: 2023/09/25 14:34:58 by yunjcho          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,111 @@
 
 Base *generate(void)
 {
-	Base	*newInstance;
 	int		rand;
 
 	rand = std::rand() % 3;
 	if (rand == 0)
 	{
-		std::cout << "A" << std::endl;
-		newInstance = new A();
+		std::cout << "Make : A" << std::endl;
+		return (new A());
 	}
 	else if (rand == 1)
 	{
-		std::cout << "B" << std::endl;
-		newInstance = new B();
+		std::cout << "Make : B" << std::endl;
+		return (new B());
 	}
 	else if (rand == 2)
 	{
-		std::cout << "C" << std::endl;
-		newInstance = new C();
+		std::cout << "Make : C" << std::endl;
+		return (new C());
 	}
 	else
 	{
 		std::cout << rand << std::endl;
-		newInstance = NULL;
 	}
-	return (newInstance);
+	return (NULL);
 }
 
-void identify(Base* p);
-void identify(Base& p);
+void identify(Base* p)
+{
+	if (dynamic_cast<A *>(p) != NULL)
+	{
+		std::cout << "Pointer : A" << std::endl;
+	}
+	else if (dynamic_cast<B *>(p) != NULL)
+	{
+		std::cout << "Pointer : B" << std::endl;
+	}
+	else if (dynamic_cast<C *>(p) != NULL)
+	{
+		std::cout << "Pointer : C" << std::endl;
+	}
+	else
+	{
+		std::cout << "Pointer : Error" << std::endl;
+	}
+}
+
+// void identify(Base& p)
+// {
+// 	if (dynamic_cast<A &>(p) != NULL)
+// 	{
+// 		std::cout << "A" << std::endl;
+// 	}
+// 	else if (dynamic_cast<B &>(p) != NULL)
+// 	{
+// 		std::cout << "B" << std::endl;
+// 	}
+// 	else if (dynamic_cast<C &>(p) != NULL)
+// 	{
+// 		std::cout << "C" << std::endl;
+// 	}
+// 	else
+// 	{
+// 		std::cout << "Error" << std::endl;
+// 	}
+// }
+
+void identify(Base& p)
+{
+	try
+	{
+		static_cast<void>(dynamic_cast<A &>(p));
+		std::cout << "Reference : A" << std::endl;
+	}
+	catch(const std::exception& e) //const std::bad_cast&
+	{
+	}
+	try
+	{
+		static_cast<void>(dynamic_cast<B &>(p));
+		std::cout << "Reference : B" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+	}
+	try
+	{
+		static_cast<void>(dynamic_cast<C &>(p));
+		std::cout << "Reference : C" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+	}
+}
 
 int main(void)
 {
-	Base	*result;
+	Base	*newInstance;
 	std::srand(static_cast<unsigned int>(time(NULL)));
-	
-	result = generate();
+
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout << i+1 << std::endl;
+		newInstance = generate();
+		identify(newInstance);
+		identify(*newInstance);
+		std::cout << std::endl;
+	}
 	return (0);
 }
