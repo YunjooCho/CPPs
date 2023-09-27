b606c5682696b109a8a98c87f8570f3f8c4e83a9
Yunjoo Cho <yunjcho@cx1r1s4.42seoul.kr>
2023-09-27 19:52:44 +0900

    CPP07 - BackUp

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:57:54 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/09/27 18:59:28 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/09/27 19:45:27 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 
 # include <iostream>
 
-template <typename T>
-void	iter(T& a, T& b, T& c)
+template <typename A, typename B, typename C>
+void	iter(A& a, B& b, C& c)
 {
-	
-}
+	// T	*arr = new T[b + 1]; //마지막에 0 넣기
+	// *a = arr[0];
+	// for (int i = 0; i < b; i++)
+	// {
+	// }
+	std::cout << "addr : " << a << std::endl;
+	std::cout << "array length : " << b << std::endl;
+	std::cout << "function addr : " << c << std::endl;
+};
 
 #endif
