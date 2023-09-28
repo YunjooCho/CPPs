4980d1c0c2af29476822916d68ef558a300813aa
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-09-28 15:35:08 +0900

    CPP07/ex01 - Update main(), iter()

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:57:54 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/09/27 21:29:10 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/09/28 15:34:10 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,28 @@
 # include <iostream>
 # include <string>
 
+template <typename T>
+T	plus(T& fir, T& sec)
+{
+	return (fir + sec);
+}
+
+
+
 template <typename T1, typename T2, typename T3>
 void	iter(T1 addr, T2 len, T3 func)
 {
 	//Debugging
-	std::cout << "addr : " << addr << std::endl;
-	std::cout << "array length : " << len << std::endl;
-	std::cout << "function addr : " << func << std::endl;
+	// std::cout << "addr : " << addr << std::endl;
+	// std::cout << "array length : " << len << std::endl;
+	// std::cout << "function addr : " << &func << std::endl;
 
-	T1	*arr = new T1[len + 1]; //마지막에 0 넣기
-	// *a = arr[0];
+	T1	*arr = new T1[len + 1]; //마지막에 0 넣기?
+	*arr = addr;
 	for (int i = 0; i < static_cast<int>(len); i++)
 	{
 		arr[i] = &func;
-		std::cout << *arr[i] << std::endl;
+		std::cout << "idx : " << i << ", value : " << *arr[i] << std::endl;
 	}
 };
 
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,23 +6,42 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 18:33:03 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/09/27 21:28:07 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/09/28 15:31:55 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int	plus(const int fir, const int sec)
-{
-	return (fir + sec);
-}
-
 int	main(void)
 {
-	int		intArr[5];
-	// char	charArr[10];
+	std::cout << "------------- Type : integer -------------" << std::endl;
+	int	intArr[5];
+	int	a = 3;
+	int	b = 5;
+	::iter(intArr, sizeof(intArr)/sizeof(intArr[0]), ::plus(a, b));
 
-	::iter(intArr, sizeof(intArr)/sizeof(intArr[0]), ::plus(3, 5));
+	std::cout << "------------- Type : char -------------" << std::endl;
+	char	charArr[10];
+	char	c = 'A';
+	char	d = '\0';
+	::iter(charArr, sizeof(charArr)/sizeof(charArr[0]), ::plus(c, d));
+
+	std::cout << "------------- Type : double -------------" << std::endl;
+	double	doubleArr[3];
+	double	e = 3.2;
+	double	f = 1.9;
+	::iter(doubleArr, sizeof(doubleArr)/sizeof(doubleArr[0]), ::plus(e, f));
+
+	std::cout << "------------- Type : string -------------" << std::endl;
+	std::string	stringArr[3];
+	std::string	g = "hi";
+
+	for (int i = 0; i < 3; i++)
+	{
+		stringArr[i] = g + std::to_string(i);
+		std::cout << "[Before] idx : " << i << ", value : " << stringArr[i] << std::endl;
+	}
+	::iter(stringArr, sizeof(stringArr)/sizeof(stringArr[0]), stringArr[0].append("A"));
 	return (0);
 }
 
