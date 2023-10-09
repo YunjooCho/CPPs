68803d320291f0aa810e071a3a7fea45fe929411
YUNJOO CHO <rollcake0317@naver.com>
2023-10-09 19:39:07 +0900

    CPP08/ex02 - Modify iterator

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 19:20:32 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/09 17:14:10 by yunjcho          ###   ########seoul.kr  */
+/*   Updated: 2023/10/09 19:37:56 by yunjcho          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,48 +17,57 @@
 # include <deque>
 # include <stack>
 
-template<typename T, typename Container = std::deque<T> >
+template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 	public:
+		typedef typename Container::iterator 	iterator;
+		typedef typename Container::size_type	size_c;
+
 		MutantStack() : std::stack<T, Container>() {};
+
 		MutantStack(const MutantStack& stack) : std::stack<T, Container>(stack)
 		{
 			*this = stack;
 		}
+
 		MutantStack& operator=(const MutantStack& stack)
 		{
 			if (this != &stack)
-			{
 				this->c = stack.c;
-				// this->c.insert(this->c.begin(), stack.c.begin(),  stack.c.end()); //안되면 for문으로 복사
-			}
 			return (*this);
 		}
+
 		~MutantStack() {}
-		void		push(const T& val)
+
+		void push(const T& value) 
 		{
-			this->c.push_back(val);
+			this->c.push_back(value);
 		}
-		void		pop(void)
+
+		void pop(void) 
 		{
 			this->c.pop_back();
 		}
-		std::size_t	size(void)
+
+		const T& top(void) const
+		{
+			return (this->c.back());
+		}
+
+		size_c size(void) const 
 		{
 			return (this->c.size());
 		}
-		const T&	top(void) const
+
+		iterator begin(void)
 		{
-			return (this->c[this->c.size() - 1]);
+			return (this->c.begin());
 		}
-		typename Container<typename T>::iterator begin()
+
+		iterator end(void)
 		{
-			return this->c.begin();
-		}
-		typename Container<typename T>::iterator end()
-		{
-			return this->c.end();
+			return (this->c.end());
 		}
 };
 
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,43 +6,86 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:35:27 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/08 14:36:32 by yunjcho          ###   ########seoul.kr  */
+/*   Updated: 2023/10/09 18:58:11 by yunjcho          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <list>
 
 int main()
 {
+	std::cout << "---------- MutantStack Test ----------" << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
 	mstack.push(17);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << mstack.top() << std::endl; //17
 
 	mstack.pop();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << mstack.size() << std::endl; // 1
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-
-	//[...]
-
 	mstack.push(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
-	++it;
+	++it;	
 	--it;
-	while (it != ite)
+	int	i = 0;
+	while (it != ite) //stack이 비어있지 않으면
 	{
-		std::cout << *it << std::endl;
+		std::cout << "MutantStack[" << i << "]: " <<  *it << std::endl;
 		++it;
+		++i;
 	}
+
+	std::cout << std::endl;
+	std::cout << "---------- std::stack Test - Use Copy Constructor ----------" << std::endl;
 	std::stack<int> s(mstack);
+	i = 0;
+	while (!s.empty())
+	{
+		// std::cout << s[i] << std::endl; // 컴파일 에러
+		std::cout << "Stack[" << i << "]: " << s.top() << std::endl;
+		s.pop();
+		++i;
+	}
+
+	std::cout << std::endl;
+	std::cout << "---------- std::list Test----------" << std::endl;
+	std::list<int> lst;
+
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << lst.back() << std::endl; //17
+
+	lst.pop_back();
+
+	std::cout << lst.size() << std::endl; // 1
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::list<int>::iterator it2 = lst.begin();
+	std::list<int>::iterator ite2 = lst.end();
+
+	++it2;
+	--it2;
+	i = 0;
+	while (it2 != ite2) //stack이 비어있지 않으면
+	{
+		std::cout << "MutantStack[" << i << "]: " <<  *it2 << std::endl;
+		++it2;
+		++i;
+	}
 	return 0;
 }
