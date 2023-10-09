602e6a3d7b54a410c20c999766de6fb49d334e77
YUNJOO CHO <rollcake0317@naver.com>
2023-10-09 17:18:50 +0900

    CPP08/ex02 - Backup before remake codes

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,16 +6,60 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 19:20:32 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/06 19:21:45 by yunjcho          ###   ########seoul.kr  */
+/*   Updated: 2023/10/09 17:14:10 by yunjcho          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-class MutantStack
-{
+# include <iostream>
+# include <deque>
+# include <stack>
 
+template<typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		MutantStack() : std::stack<T, Container>() {};
+		MutantStack(const MutantStack& stack) : std::stack<T, Container>(stack)
+		{
+			*this = stack;
+		}
+		MutantStack& operator=(const MutantStack& stack)
+		{
+			if (this != &stack)
+			{
+				this->c = stack.c;
+				// this->c.insert(this->c.begin(), stack.c.begin(),  stack.c.end()); //안되면 for문으로 복사
+			}
+			return (*this);
+		}
+		~MutantStack() {}
+		void		push(const T& val)
+		{
+			this->c.push_back(val);
+		}
+		void		pop(void)
+		{
+			this->c.pop_back();
+		}
+		std::size_t	size(void)
+		{
+			return (this->c.size());
+		}
+		const T&	top(void) const
+		{
+			return (this->c[this->c.size() - 1]);
+		}
+		typename Container<typename T>::iterator begin()
+		{
+			return this->c.begin();
+		}
+		typename Container<typename T>::iterator end()
+		{
+			return this->c.end();
+		}
 };
 
 #endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 14:35:27 by yunjcho           #+#    #+#             */
+/*   Updated: 2023/10/08 14:36:32 by yunjcho          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+
+	//[...]
+
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
