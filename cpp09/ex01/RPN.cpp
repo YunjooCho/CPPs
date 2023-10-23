199f9b861783a65668a9084a516ea06a88cb32c5
YUNJOO CHO <rollcake0317@naver.com>
2023-10-23 23:31:39 +0900

    CPP09/ex02 - Delete if() about number elements

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 22:07:48 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/23 23:09:12 by yunjcho          ###   ########seoul.kr  */
+/*   Updated: 2023/10/23 23:30:08 by yunjcho          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,21 +41,26 @@ bool	RPN::isOperator(char c)
 
 void	RPN::calculate(const std::string& argv)
 {
+	int	operCnt = 0;
+
 	for (size_t i = 0; i < argv.length(); i++)
 	{
 		//Debugging
-		std::cout << "argv[" << i << "] : " << argv[i] << std::endl;
+		// std::cout << "argv[" << i << "] : " << argv[i] << std::endl;
+		// if (_stack.size() > 0)
+		// 	std::cout << _stack.top() << std::endl;
+
 		if (argv[i] == ' ')
 			continue ;
 		else if (std::isdigit(argv[i]))
 		{
-			// if (_stack.size() == 2) // 옵션이 와야 함
+			// if (_stack.size() == 2) // 옵션이 와야 함 //TODO - "1 2 * 2 / 2 * 2 4 - +"
 			// 	throw std::runtime_error("Error: already enough numbers elements.");
-			// else
-				_stack.push(argv[i] - '0');
+			_stack.push(argv[i] - '0');
 		}
-		else if (isOperator(argv[i]))
+		else if (isOperator(argv[i]) == true)
 		{
+			++operCnt;
 			if (_stack.size() < 2)
 				throw std::runtime_error("Error: not enough numbers elements.");
 			int num2 = _stack.top();
@@ -70,11 +75,17 @@ void	RPN::calculate(const std::string& argv)
 			{
 				if (num2 == 0)
 					throw std::runtime_error("Error: You cannot divide by zero.");
-				_stack.push(num1/num2);
+				_stack.push(num1 / num2);
+			}
+			else if (argv[i] == '*')
+			{
+				_stack.push(num1 * num2);
 			}
 		}
 		else
 			throw std::runtime_error("Error");
 	}
+	if (operCnt == 0)
+		throw std::runtime_error("Error: At least one operator is required.");
 	std::cout << _stack.top() << std::endl;
 }
