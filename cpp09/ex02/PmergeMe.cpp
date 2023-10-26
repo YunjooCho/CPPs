573232cd9194cf1245bc83e0e94c9b913718e486
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-10-26 22:19:00 +0900

    CPP09/ex02 - Delete Set -> use List's sort()

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 22:09:28 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/26 21:56:19 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/10/26 22:18:09 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,6 @@
 PmergeMe::PmergeMe() {}
 
 PmergeMe::~PmergeMe() {}
-
-// PmergeMe::Pair::Pair(int x, int y) : _x(x), _y(y) {}
-
-// PmergeMe::Pair::~Pair() {}
-
-// bool  PmergeMe::Pair::operator<(const Pair& pair) const
-// {
-// 	return (_x < pair._x || (_x == pair._x && _y < pair._y));
-// }
 
 //private
 PmergeMe& PmergeMe::operator=(const PmergeMe& instance) 
@@ -40,24 +31,6 @@ PmergeMe::PmergeMe(const PmergeMe& instance)
 {
 	*this = instance;
 }
-
-//private
-// PmergeMe::Pair::Pair() {}
-
-// //private
-// PmergeMe::Pair& PmergeMe::Pair::operator=(const Pair& pair) 
-// {
-// 	if (this != &pair)
-// 	{
-// 		_x = pair._x;
-// 		_y = pair._y;
-// 	}
-// 	return (*this);
-// }
-
-// //private
-// PmergeMe::Pair::Pair(const Pair& pair) {}
-
 
 void	PmergeMe::parsing(char **argv)
 {
@@ -91,69 +64,7 @@ void	PmergeMe::parsing(char **argv)
 
 }
 
-// void	PmergeMe::pairSort(void)
-// {
-// 	size_t	targetIdx = _con.size() - 2;
-
-// 	if (_con.size() % 2 != 0)
-// 		targetIdx -= 1;
-
-// 	std::list<int>::iterator iter = _con.begin();
-// 	std::list<int>::iterator iter2 = ++_con.begin();
-// 	for (size_t i = 0; i <= targetIdx; i += 2)
-// 	{
-// 		if (*iter < *iter2)
-// 		{
-// 			_sortCon.push_back(*iter2);
-// 			_sortCon.push_back(*iter);
-// 		}
-// 		else
-// 		{
-// 			_sortCon.push_back(*iter);
-// 			_sortCon.push_back(*iter2);
-// 		}
-// 		for (size_t j = 0; j < 2; j++)
-// 		{
-// 			++iter;
-// 			++iter2;
-// 			_con.pop_front();
-// 		}
-// 	}
-	
-// 	if (_con.size() > 0)
-// 	{
-// 		while (iter != _con.end())
-// 		{
-// 			//debugging
-// 			// std::cout << "!iter : " << *iter << std::endl;
-// 			_sortCon.push_back(*iter);
-// 			++iter;
-// 		}		
-// 	}
-
-// 	//debugging
-// 	for (std::list<int>::iterator iter = _sortCon.begin(); iter != _sortCon.end(); iter++)
-// 	{
-// 		std::cout << *iter << " ";
-// 	}
-// 	std::cout << std::endl;
-// }
-
-// void	PmergeMe::mainChainSort(void)
-// {
-// 	size_t		targetIdx = _con.size() - 2;
-// 	int			val1 = -1;
-// 	int 		val2 = -1;
-// 	std::list<int> tmp;
-
-// 	(void) val1;
-// 	(void) val2;
-// 	(void) tmp;
-// 	if (_sortCon.size() % 2 != 0)
-// 		targetIdx -= 1;
-// }
-
-void	PmergeMe::pairSort(void)
+void	PmergeMe::createMainchain(void)
 {
 	size_t			targetIdx = _con.size() - 2;
 	std::list<int>	_copyCon(_con);
@@ -165,9 +76,11 @@ void	PmergeMe::pairSort(void)
 	for (size_t i = 0; i <= targetIdx; i += 2)
 	{
 		if (*iter < *iter2)
-			_mainChain.insert(std::make_pair(*iter2, *iter));
+			_mainChain.push_back(std::make_pair(*iter2, *iter));
+			// _mainChain.insert(std::make_pair(*iter2, *iter));
 		else if(*iter > *iter2)
-			_mainChain.insert(std::make_pair(*iter, *iter2));
+			_mainChain.push_back(std::make_pair(*iter, *iter2));
+			// _mainChain.insert(std::make_pair(*iter, *iter2));
 		else
 			throw std::runtime_error("Error: Duplicate values existed.");
 		for (size_t j = 0; j < 2; j++)
@@ -182,23 +95,33 @@ void	PmergeMe::pairSort(void)
 	{
 		while (iter != _copyCon.end())
 		{
-			_mainChain.insert(std::make_pair(2147483647, *iter));
+			// _mainChain.insert(std::make_pair(2147483647, *iter));
+			_mainChain.push_back(std::make_pair(2147483647, *iter));
 			++iter;
 		}		
 	}
 
-	//debugging
-	for (std::set<std::pair<int, int> >::iterator iter = _mainChain.begin(); iter != _mainChain.end(); iter++)
+	// debugging
+	// for (std::set<std::pair<int, int> >::iterator iter = _mainChain.begin(); iter != _mainChain.end(); iter++)
+	// {
+	// 	std::cout << "set key : " << iter->first << ", value : " << iter->second << std::endl;
+	// }
+	_mainChain.sort();
+	for (std::list<std::pair<int, int> >::iterator iter = _mainChain.begin(); iter != _mainChain.end(); iter++)
 	{
 		std::cout << "set key : " << iter->first << ", value : " << iter->second << std::endl;
 	}
 }
 
+// void	PmergeMe::mergeInsertionSort(void)
+// {
+	
+// }
+
 void	PmergeMe::sort(void)
 {
 	if (_con.size() == 1)
 		return ;
-	pairSort();
-	// mainChainSort();
+	createMainchain();
 	// mergeInsertionSort();
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 22:02:48 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/26 21:44:00 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/10/26 22:15:35 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,12 @@ class PmergeMe
 		PmergeMe();
 		~PmergeMe();
 		void	parsing(char **argv);
-		void	pairSort(void);
-		// void	mainChainSort(void);
+		void	createMainchain(void);
 		// void	mergeInsertionSort(void);
 		void	sort(void);
-		// class Pair
-		// {
-		// 	public:
-		// 		int	_x;
-		// 		int	_y;
-		// 		Pair(int x, int y);
-		// 		~Pair();
-		// 	private:
-		// 		Pair();
-		// 		Pair(const Pair& pair);
-		// 		Pair& operator=(const Pair& pair);
-		// 		// bool  operator<(const Pair& pair) const;
-		// };
 	private:
-		// typedef std::pair<int, int> 	_setPairs;
-		std::set<std::pair<int, int> >	_mainChain;
+		// std::set<std::pair<int, int> >	_mainChain;
+		std::list<std::pair<int, int> > _mainChain;
 		std::list<int>					_con;
 		PmergeMe(const PmergeMe& instance);
 		PmergeMe& operator=(const PmergeMe& instance);
