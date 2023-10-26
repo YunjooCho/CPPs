b190ac149ad6ef9063977ac559882b40bca58e39
Yunjoo Cho <yunjcho@c1r7s6.42seoul.kr>
2023-10-26 21:56:59 +0900

    CPP09/ex02 - Modify pairSort() : Use Set Container

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 22:02:48 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/10/26 19:50:12 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/10/26 21:44:00 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <deque> //TODO - 추후 불필요한 경우 삭제
 # include <sstream>
 # include <string>
+# include <set>
 
 class PmergeMe
 {
@@ -26,12 +27,26 @@ class PmergeMe
 		~PmergeMe();
 		void	parsing(char **argv);
 		void	pairSort(void);
-		void	mainChainSort(void);
+		// void	mainChainSort(void);
 		// void	mergeInsertionSort(void);
 		void	sort(void);
+		// class Pair
+		// {
+		// 	public:
+		// 		int	_x;
+		// 		int	_y;
+		// 		Pair(int x, int y);
+		// 		~Pair();
+		// 	private:
+		// 		Pair();
+		// 		Pair(const Pair& pair);
+		// 		Pair& operator=(const Pair& pair);
+		// 		// bool  operator<(const Pair& pair) const;
+		// };
 	private:
-		std::list<int>	_con;
-		std::list<int>	_sortCon;
+		// typedef std::pair<int, int> 	_setPairs;
+		std::set<std::pair<int, int> >	_mainChain;
+		std::list<int>					_con;
 		PmergeMe(const PmergeMe& instance);
 		PmergeMe& operator=(const PmergeMe& instance);
 };
