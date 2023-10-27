22988d17e444c8ceae773b0dc14a8e7d862673c9
Yunjoo Cho <yunjcho@c1r7s7.42seoul.kr>
2023-10-27 22:21:13 +0900

    CPP04/ex03 - Modify main()

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 17:07:25 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/08/31 15:27:07 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/09/01 14:07:13 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,75 @@
 
 int main()
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
-	
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	
-	ICharacter* bob = new Character("bob");
+		ICharacter* me = new Character("me");
+		
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		
+		ICharacter* bob = new Character("bob");
 
-	me->use(0, *bob);
-	me->use(1, *bob);
-	me->use(2, *bob); //anything
-	
-	me->unequip(0);
-	me->unequip(1);
-	me->unequip(2);  //anything
-	
-	delete bob;
-	delete me;
-	delete src;
+		me->use(0, *bob);
+		me->use(1, *bob);
+		me->use(2, *bob); //anything
+		
+		me->unequip(0);
+		me->unequip(1);
+		me->unequip(2);  //anything
+		
+		delete bob;
+		delete me;
+		delete src;
+	}
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
+		ICharacter* me = new Character("me");
+		
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter* bob = new Character("bob");
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+		me->use(2, *bob); //anything
+		
+		me->unequip(0);
+		me->unequip(1);
+		me->unequip(2);  //anything
+		me->unequip(3);
+
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+
+		me->unequip(0);
+		me->unequip(1);
+
+		delete bob;
+		delete me;
+		delete src;
+	}
 	return 0;
 }
